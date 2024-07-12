6fae02431db7fb7c3bd5af0ce23abb8773a0af45
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-07-12 11:01:31 +0500

    add cills

---
+++ src/tic_tac_toe game/components/box/index.jsx
@@ -0,0 +1,9 @@
+import "../../style.css";
+function Cell(proms) {
+  return (
+    <>
+      <div className="game__box-wrapper"></div>
+    </>
+  );
+}
+export default Cell;
--- src/tic_tac_toe game/index.jsx
+++ src/tic_tac_toe game/index.jsx
@@ -1,17 +1,46 @@
+import { useLocation } from "react-router-dom";
+import Cell from "./components/box";
 import "./style.css";
 function Game() {
+  const { pathname } = useLocation();
+  const cell = [
+    {
+      id: "1",
+    },
+    {
+      id: "2",
+    },
+    {
+      id: "3",
+    },
+    {
+      id: "4",
+    },
+    {
+      id: "5",
+    },
+    {
+      id: "6",
+    },
+    {
+      id: "7",
+    },
+    {
+      id: "8",
+    },
+    {
+      id: "9",
+    },
+  ];
+  {
+    console.log(cell[1].id);
+  }
   return (
     <>
       <div className="game__wrapper">
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
-        <div className="game__box-wrapper"></div>
+        {cell.map((item) => (
+          <Cell key={item.id} />
+        ))}
       </div>
     </>
   );
